267b0d296a0f9d0ecdbc9f3153d172423c5777af
0J3J8A <jaguirremarin@byupathway.edu>
2025-05-08 12:44:29 -0500

    adding week01 assigment (html/js/css/images/folders)

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Current Year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastmodification").textContent = lastModified;
